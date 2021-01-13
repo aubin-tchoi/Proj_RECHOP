439c55f2429747a5f1eeb30717b2ad2e575fb4ff
aubin-tchoi <aubin.tchoi@gmail.com>
2021-01-13 11:56:12 +0100

    plne

--- Work/main.jl
+++ Work/main.jl
@@ -1,11 +1,5 @@
 ########################### Formation de clusters par région ######################
-import Pkg
-Pkg.add("LightGraphs")
-Pkg.add("ProgressMeter")
-ENV["GUROBI_HOME"] = "/Library/gurobi911/mac64"
-import Pkg
-Pkg.add("Gurobi")
-Pkg.build("Gurobi")
+
 
 #INCLUDES
 include("dimensions.jl")
@@ -20,15 +14,18 @@ include("write.jl")
 include("clusters.jl")
 include("plne.jl")
 
-instance = lire_instance("Work/instances/europe.csv")
+instance = lire_instance("Work/instances/maroc.csv")
 
 ######################### Variables ###############################
 taille_max_clusters = 4                       #Hors usine #attention petite.csv n'a que 3 fournisseurs et il faut taille_clusters<=instance.F
 #global Nbr_clusters = ceil(Int, instance.F / taille_max_clusters)    #min = part_ent_sup(instance.F/taille_clusters) le coût d'introduction d'une nouvelle route est 150* plus importante que le coût kilométrique. Il est donc probable qu'il soit plus intéressant de prendre le nbr de route minimum qui à ce que chaque camion parcours plus de distance.
 Nbr_regions_x = 8
 Nbr_regions_y = 3
-Time_max_optimization = 60        #in sec
+Time_max_optimization = 1        #in sec
 
 
 clusters = makeClusters(instance, Nbr_regions_x, Nbr_regions_y)
-plne_sol = solve_plne(instance, clusters) 
+
+println("CLUSTERS OK")
+plne_sol = solve_plne(instance, formatClusters(instance, clusters), 300) 
+
--- Work/plne.jl
+++ Work/plne.jl
@@ -11,13 +11,13 @@ include("route.jl")
 include("solution.jl")
 include("write.jl")
 
-function formatClusters(clusters::Array{Array{Int64,2},1})
+function formatClusters(instance::Instance, clusters::Array{Array{Int64,2},1})
     newClusters = Array{Array{Int64,1},1}(undef, 0)
-    for cluster in clusters
-        for f in 1:size(cluster, 1)
-            newCluster = Array{Int64,1}(undef, 0)
-            if cluster[f] == 1
-                push!(newCluster, f)
+    for c = 1:size(clusters, 1)
+        newCluster = Array{Int64,1}(undef, 0)
+        for f in 1:size(clusters[c], 2)
+            if clusters[c][1,f] == 1
+                push!(newCluster, f + instance.U)
             end
         end
         push!(newClusters, newCluster)
@@ -25,48 +25,133 @@ function formatClusters(clusters::Array{Array{Int64,2},1})
     return newClusters
 end
 
-
-function solve_plne(instance::Instance, clusters::Array{Array{Int64,2},1})
-    sol_clusters = Array{Array{Int64,4}, 1}
+function solve_plne(instance::Instance, clusters::Array{Array{Int64,1},1}, K::Int64)
+    sol_clusters = Array{Array{Int64,5}, 1}(undef, size(clusters, 1))
+    
     for e = 1:instance.E
-        model = Model(Gurobi.Optimizer)
-
+        println("DEBUT OK")
+        model = Model(with_optimizer(Gurobi.Optimizer,  TimeLimit = 10))
+        println("MODEL OK")
         #= On sépare par clusters i, ensuite la 1ère coord correspond au jour, la 2e au départ et la 3e à la destination
         Dans le départ et la destination on met les 4 fournisseurs en premiers, suivi des instance.U usines
-        (d[i, j, 1, 2]) == 1 si dans le cluster i un camion va du 1er fournisseur au 2e =#
+        (d[i, j, k, 1, 2]) == 1 si dans le cluster i le camion k va du 1er fournisseur au 2e =#
 
-        @variable(model, d[1:size(clusters, 1), 1:instance.J, 1:(4 + instance.U), 1:(4 + instance.U)], Bin)
-        @variable(model, x[1:size(clusters, 1), 1:instance.J, 1:(4 + instance.U), 1:(4 + instance.U)], Int)
+        @variable(model, d[1:size(clusters, 1), 1:instance.J, 1:K, 1:(4 + instance.U), 1:(4 + instance.U)], Bin)
+        @variable(model, x[1:size(clusters, 1), 1:instance.J, 1:K, 1:(4 + instance.U), 1:(4 + instance.U)], Int)
         @variable(model, s[1:size(clusters, 1), 1:instance.J, 1:(4 + instance.U)], Int)
         @variable(model, s1[1:size(clusters, 1), 1:instance.J, 1:(4 + instance.U)], Int)
         @variable(model, s2[1:size(clusters, 1), 1:instance.J, 1:(4 + instance.U)], Int)
 
+        println("VARIABLES OK")
+
+        # delta
         for i = 1:size(clusters, 1)
             for j = 1:instance.J
-                for a = 1:(instance.J + 4)
-                    for b = 1:(instance.J + 4)
-                        @constraint(model, x[i, j, a, b] <= instance.L * d[i, j, a, b])
-                        @constraint(model, x[i, j, a, b] >= d[i, j, a, b])
-                        @constraint(model, x[i, j, a, b] >= 0)
+                for k = 1:K
+                    for a = 1:(instance.U + 4)
+                        for b = 1:(instance.U + 4)
+                            # McCormick
+                            @constraint(model, x[i, j, k, a, b] <= instance.L * d[i, j, k, a, b])
+                            @constraint(model, x[i, j, k, a, b] >= d[i, j, k, a, b])
+                            @constraint(model, x[i, j, k, a, b] >= 0)
+                        end
+                        # On ne se livre pas à soi-même
+                        @constraint(model, d[i, j, k, a, a] == 0)
+
+                        # En partant d'un sommet on peut aller vers 1 seul autre sommet (au plus 1 destination)
+                        @constraint(model, sum(d[i, j, k, a, b] for b = 1:(instance.U + 4)) <= 1)
+
+                        # 0 si jamais le cluster contient moins de 4 éléments
+                        @constraint(model, sum(d[i, j, k, a, b] for b = (size(clusters[i], 1) + 1):4) == 0)
+                        @constraint(model, sum(d[i, j, k, b, a] for b = (size(clusters[i], 1) + 1):4) == 0)
+
                     end
                 end
             end
         end
+
+        # Stocks
+        for i = 1:size(clusters, 1)
+            for j = 1:instance.J
+                for u = 1:instance.U
+                    # s' pour linéariser le max(s-r, 0)
+                    @constraint(model, s1[i, j, u + 4] >= instance.usines[u].r[e, j])
+                end
+                for a = 1:(4 + instance.U)
+                    # s' et s"
+                    @constraint(model, s1[i, j, a] >= s[i, j, a])
+                    @constraint(model, s2[i, j, a] >= s[i, j, a])
+                end
+            end
+        end
+
+        # Usines
+        for i = 1:size(clusters, 1)
+            for j = 1:instance.J
+                for u = 1:instance.U
+                    # Stock positif pour les usines
+                    @constraint(model, s[i, j, u + 4] >= 0)
+                    # Évolution du stock
+                    if j == 1
+                        @constraint(model, s[i, j, u + 4] == instance.usines[u].s0[e])
+                    elseif j >= 2
+                        @constraint(model, s[i, j, u + 4] - s[i, j - 1, u + 4] + sum(sum(x[i, j, k, u, b] for b = 1:(4 + instance.U)) for k = 1:K) == instance.usines[u].b⁺[e, j])
+                    end
+                end
+            end
+        end
+
+        println(size(instance.fournisseurs))
+        println(size(clusters))
+        println(size(instance.fournisseurs[1].b⁻))
+
+        # Fournisseurs
+        for i = 1:size(clusters, 1)
+            for j = 1:instance.J
+                for f = 1:4
+                    if (j >= 2)
+                        println(clusters[i][f])
+                        println(instance.fournisseurs[clusters[i][f]])
+                        println(instance.fournisseurs[clusters[i][f]].v)
+                        println(instance.fournisseurs[clusters[i][f]].b⁻[e, j])
+                        @constraint(model, s[i, j, f] - sum(sum((x[i, j, k, a, f] - x[i, j, k, f, a]) for a = 1:(4 + instance.U)) for k = 1:K) >= s[i, j - 1, f] - instance.fournisseurs[clusters[i][f]].b⁻[e, j])
+                        @constraint(model, s2[i, j - 1, f] >= instance.fournisseurs[clusters[i][f]].b⁻[e, j])
+                    end
+                    @constraint(model, s[i, j, f] - sum(sum((x[i, j, k, a, f] - x[i, j, k, f, a]) for a = 1:(4 + instance.U)) for k = 1:K) >= 0)
+                end
+            end
+        end
+
+        # Pas plus de 4 fournisseurs par camion
+        for i = 1:size(clusters, 1)
+            for j = 1:instance.J
+                for k = 1:K
+                    @constraint(model, sum(sum(d[i, j, k, a, b] for a = 1:(4 + instance.U)) for b = 1:(4 + instance.U)) <= 4)
+                end
+            end
+        end
+
+        println("CONSTRAINTS OK")
 
         @objective(
             model,
             Min,
             sum(
                 sum(
-                    sum(sum(d[i, j, a, b] * (instance.γ * 1 + instance.cstop)) for a = 1:(4 + instance.U)) for b = 1:(4 + instance.U)
+                    sum(sum(sum(d[i, j, k, 4 + u, f] * (instance.γ * instance.graphe.d[instance.usines[u].v, instance.fournisseurs[clusters[i][f]].v] + instance.cstop) for u = 1:instance.U) for f = 1:4) for k = 1:K)
                     +
-                    sum(instance.usines[u - 4].cs[e] * (s1[i, j, u] - instance.usines[u - 4].r[e, j])) for u = 5:(4 + instance.U)
+                    sum(sum(sum(d[i, j, k, f1, f2] * (instance.γ * instance.graphe.d[instance.usines[u].v, instance.fournisseurs[clusters[i][f]].v] + instance.cstop) for f1 = 1:4) for f2 = 1:4) for k = 1:K)
+                    +
+                    sum(instance.usines[u].cs[e] * (s1[i, j, u + 4] - instance.usines[u].r[e, j]) for u = 1:instance.U)
                     +
                     sum(instance.fournisseurs[clusters[i][f]].cs[e] * (s1[i, j, f] - instance.fournisseurs[clusters[i][f]].r[e, j])
-                    + instance.fournisseurs[clusters[i][f]].cexc[e] * (s2[i, j, f] - s[i, j, f])) for f = 1:4
-                ) for j = 1:instance.J
-            ) for i = 1:size(clusters, 1)
+                    + instance.fournisseurs[clusters[i][f]].cexc[e] * (s2[i, j, f] - s[i, j, f]) for f = 1:4)
+                for j = 1:instance.J)
+            for i = 1:size(clusters, 1)
+            )
         )
+    
+        println("OBJECTIVE OK")
 
         JuMP.optimize!(model)
 
@@ -74,14 +159,15 @@ function solve_plne(instance::Instance, clusters::Array{Array{Int64,2},1})
             for i = 1:size(clusters, 1)
                 for a = 1:(4 + instance.U)
                     for b = 1:(4 + instance.U)
-                        sol_clusters[i][e, j, a, b] = JuMP.value(x[i, j, a, b])
+                        for k = 1:K
+                            sol_clusters[i][e, j, k, a, b] = JuMP.value(x[i, j, k, a, b])
+                        end
                     end
                 end
             end
         end
     end
-    println("Objectif : ")
-    println(JuMP.objective_value(model))
+    #println(JuMP.objective_value(model))
     return sol_clusters
 end
 
